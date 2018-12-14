3ee3f43c204aa79aecb402d5f21b66727344e080
Sarah Cooke <marshcooke1@gmail.com>
2018-12-14 15:21:02 -0600

    dependencies upgrade and more maybe

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,18 +1,2 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'NgMap']);
 
-myApp.controller('BreweryController', function ($http) {
-    var vm = this;
-    vm.info = { list: [] };
-    vm.getInfo = function () {
-        $http({
-            method: 'GET',
-            url: '/brewery'
-        }).then(function (response) {
-            console.log('Received', response);
-            vm.info.list = response.data.data;
-            console.log(vm.info.list);
-        });
-    }
-
-    vm.getInfo();
-});
--- server.js
+++ server.js
@@ -4,42 +4,10 @@ require('dotenv').config();
 var app = express();
 var port = process.env.PORT || 4424;
 
-app.use(express.static('server/public'));
-
-// Using requst module to make HTTP requests from the server
-// https://www.npmjs.com/package/request
-var request = require('request');
-
-var base = "http://api.brewerydb.com/v2";
-var search = "/search?q=";
-var brewery = 'Surly';
-var type = "&type=brewery";
-var keyHead = "&key=";
-var key = process.env.API_KEY;
-var withLocations = '&withLocations=y';
-var fullUrl = base + search + brewery + type +  withLocations + keyHead + key;
 
 app.use(express.static('public'));
 
-app.get('/brewery', function (req, res) {
-    request(fullUrl, function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode: ', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body: ', body); // Print the HTML for the Google homepage.
 
-        res.status(200).send(JSON.parse(body));         
-    })
-})
-
-// app.get('/UPDATE', function(req, res) {
-//     request(fullUrl, function(error, response, body) {
-//         console.log('error:', error);
-//         console.log('statusCode: ', response && response.statusCode);
-//         console.log('body: ', body);
-
-//         res.status(200).send(JSON.parse(body));
-//     })
-// })
 
 
 
